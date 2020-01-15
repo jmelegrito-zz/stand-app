43a116ef8e5e7f2b386e45d11b343d006b0dd8e0
John Melegrito <john_melegrito@yahoo.com>
2020-01-14 19:56:45 -0600

    working local strategy

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 
 const express = require('express');
 const app = express();
-
+const models = require('./models');
+const session = require("express-session");
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+var pbkdf2 = require('pbkdf2');
+var salt = "XZoLh12Teu";
 
-const port = process.env.PORT || 3000;
+function encryptionPassword(password) {
+  var key = pbkdf2.pbkdf2Sync(
+    password, salt, 36000, 256, 'sha256'
+  );
+  var hash = key.toString('hex');
+
+  return hash;
+}
+
+app.use(session({
+  secret: "cats", 
+  resave: false, 
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/public'));
+
+const port = 3000;
 app.listen(port , () => console.log('App listening on port ' + port));
-
-
-
-
 
 /*  PASSPORT SETUP  */
 
@@ -21,8 +37,25 @@ const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
-app.get('/error', (req, res) => res.send("error logging in"));
+app.get('/success', function (req, res) {
+  if(req.isAuthenticated()) {
+    res.send("Welcome " + req.user.username + "!!");
+  } else {
+    res.send("You are not authorized to access this page.");
+  }
+});
+
+app.get('/error', function(req, res) {res.send("There was an error logging you in. Please try again later.")});
+
+app.get('/logout', function(req, res) {
+  if(req.isAuthenticated()){
+    console.log("The user is logging out.");
+    req.logOut();
+    res.send("The user has logged out.");
+  } else {
+    res.send("You don't have a session open.");
+  }
+});
 
 passport.serializeUser(function(user, cb) {
   cb(null, user.id);
@@ -43,23 +76,23 @@ passport.deserializeUser(function(id, cb) {
 const LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-      UserDetails.findOne({
+  function (username, password, done) {
+    models.user.findOne({
+      where: {
         username: username
-      }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
+      }
+    }).then(function (user) {
+      if (!user) {
+        return done(null, false);
+      }
 
-        if (!user) {
-          return done(null, false);
-        }
-
-        if (user.password != password) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
+      if (user.password != encryptionPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    }).catch(function (err) {
+      return done(err);
+    });
   }
 ));
 
@@ -67,4 +100,55 @@ app.post('/',
   passport.authenticate('local', { failureRedirect: '/error' }),
   function(req, res) {
     res.redirect('/success?username='+req.user.username);
+  });
+
+  app.post("/sign-up", function (req, response) {
+    models.user.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username, 
+      password: encryptionPassword(req.body.password),
+      email: req.body.email,
+      groupsID: req.body.groupsID
+    })
+      .then(function (user) {
+        response.send(user);
+      });
+  });
+
+  app.post("/group-sign-up", function (req, response) {
+    models.group.create({
+      groupName: req.body.groupName
+    })
+      .then(function (user) {
+        response.send(user);
+      });
+  });
+
+
+
+//GoogleStrategy template OAuth2.0 API
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: "test",
+    clientSecret: "test",
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+))
+
+//Express google template
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
   });
